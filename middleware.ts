7a02cf799f287fb2cec36dcda6421bf9605c65e6
David Corson <53447317+dcorso21@users.noreply.github.com>
2022-12-13 13:52:25 -0500

    update

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,8 @@
 import { next } from "@vercel/edge";
 
 export default async function middleware() {
-    try {
-        return next({
-            headers: { "x-from-middleware": "true" },
-        });
-    } catch (e) {
-        console.log(e);
-        throw new Error("Unable to set headers");
-    }
+    console.log('Middleware function')
+    return next({
+        headers: { "x-from-middleware": "true" },
+    });
 }
